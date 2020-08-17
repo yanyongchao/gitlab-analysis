0b19182f75d4919e7e3ae8c38ee51548a0f65c04
yanyongchao <yongchao.yan@welian.com>
2020-08-17 11:44:21 +0800

    修改webhook

---
+++ app/constant/user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const userMap = {
+  晏永超: '15158994593',
+  郭晓婷: '13758141861',
+  邵雯芬: '18767158206',
+};
+
+module.exports = userMap;
--- app/controller/webhook.js
+++ app/controller/webhook.js
@@ -1,18 +1,41 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const ChatBot = require('dingtalk-robot-sender');
+const userMap = require('../constant/user');
+
+const robot = new ChatBot({
+  baseUrl: 'https://oapi.dingtalk.com/robot/send',
+  accessToken: '66dc633cef834675ff2687d412be039670742c949a53d86140ad8eff16202b24',
+  secret: 'SEC6cfaf6231273cf8e4b8e1b1f10c3e3746358da75ff139566ce4fc4a7136d5397',
+});
 
 class WebhookController extends Controller {
   async index() {
     const { ctx } = this;
-    console.log(ctx.request.body);
-    ctx.logger.info('request data: %j', ctx.request.body);
-    ctx.body = {
-      success: true,
-      data: {
-        name: 'yyc',
+    const { object_attributes: { action, url }, assignee: { name } } = ctx.request.body;
+    const mobile = userMap[name];
+    let content;
+    if (action === 'open') {
+      content = `你有一个请求待合并，链接：${url}`;
+    }
+    const textContent = {
+      msgtype: 'text',
+      text: {
+        content,
+      },
+      at: {
+        atMobiles: [
+          mobile,
+        ],
+        isAtAll: false,
       },
     };
+    robot.send(textContent);
+    ctx.body = {
+      success: true,
+    };
+    ctx.logger.info('request data: %j', ctx.request.body);
   }
 }
 
